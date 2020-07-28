5fca48b5f4f8d4ce088561602cd4643473fc199b
Afia Siaw <afiasiaw@gmail.com>
2020-07-28 13:54:41 -0400

    updated user profile

--- src/App.js
+++ src/App.js
@@ -35,8 +35,27 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+      },
     };
   }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   calculateFaceLocation = (data) => {
     const clarifaiFace =
@@ -61,15 +80,30 @@ class App extends Component {
     this.setState({ input: event.target.value });
   };
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({
       imageUrl: this.state.input,
     });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then((response) =>
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      .then((response) => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              console.log('count', count);
+              this.setState(Object.assign(this.state.user, { entries: count }));
+              console.log('check', this.state.user);
+            });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
       .catch((err) => console.log(err));
   };
 
@@ -83,7 +117,13 @@ class App extends Component {
   };
 
   render() {
-    const { isSignedIn, box, imageUrl, route } = this.state;
+    const {
+      isSignedIn,
+      box,
+      imageUrl,
+      route,
+      user: { name, entries },
+    } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
@@ -94,18 +134,21 @@ class App extends Component {
         {route === 'home' ? (
           <div>
             <Logo />
-            <Rank />
+            <Rank name={name} entries={entries} />
             <ImageLinkForm
               onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit}
+              onPictureSubmit={this.onPictureSubmit}
             />
 
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
         ) : route === 'signin' ? (
-          <Signin onRouteChange={this.onRouteChange} />
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
-          <Register onRouteChange={this.onRouteChange} />
+          <Register
+            onRouteChange={this.onRouteChange}
+            loadUser={this.loadUser}
+          />
         )}
       </div>
     );
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -27,8 +27,9 @@ class Signin extends Component {
       }),
     })
       .then((res) => res.json())
-      .then((data) => {
-        if (data === 'successful login') {
+      .then((user) => {
+        if (user.id) {
+          this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
       });
